2a027e784f7da04a5ed37917bd99d044f346b9e3
Sebastián Cruz <sebascruz.0410@gmail.com>
2026-06-06 12:58:58 -0500

    chore: eliminar archivo temporal de depuración check_tok.py

--- backend/check_tok.py
+++
@@ -1,6 +0,0 @@
-from huggingface_hub import hf_hub_download 
-import json 
-path = hf_hub_download('SebastianCruz10/fakenews-mrbert-es-E1', 'tokenizer_config.json') 
-config = json.load(open(path)) 
-print(config.get('tokenizer_class')) 
-print(config.get('model_type')) 
